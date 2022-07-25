4f16d9ab8625f9b3efe2d6eb9eff0a64ef6b278d
JeancaG <98427356+JeancaG@users.noreply.github.com>
2022-07-24 20:50:47 -0600

    Primer commit del proyecto

---
+++ config/Conexion.php
@@ -0,0 +1,19 @@
+<?php
+    class Conectar{
+        protected $dbh;
+
+        protected function Conexion(){
+            try{
+                $conectar = $this->dbh = new PDO("mysql-host=20.216.41.245;dbname=g7_19","G7_19","dFVSi8q7");
+                return $conectar;
+            } 
+            catch(Exception $e) {
+                print "Error BD!: " . $e->getMessage() . "<br/>";
+                die();
+            }
+        }
+        public function set_names(){
+            return $this->dbh->query("SET NAMES 'utf8'");
+        }
+    }
+    ?>
